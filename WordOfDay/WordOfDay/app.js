5629e87b4c445b9ac07e2618b00f40f821ed4bc3
vchalyy <1st.still@gmail.com>
2016-04-09 19:59:48 +0300

    change template engine, add bootstrap

--- WordOfDay/WordOfDay/app.js
+++ WordOfDay/WordOfDay/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var handlebars = require('express-handlebars');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,8 +12,8 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', handlebars({ defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -20,8 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(__dirname + '/public'));
 
 app.use('/', routes);
 app.use('/users', users);
