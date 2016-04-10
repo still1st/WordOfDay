fa239a52fd4c36f4f39da3aea8d56a8266d06b8a
vchalyy <1st.still@gmail.com>
2016-04-10 10:52:51 +0300

    add mongoose and config

--- WordOfDay/routes/index.js
+++ WordOfDay/routes/index.js
@@ -1,5 +1,6 @@
 ﻿var express = require('express');
 var router = express.Router();
+var repository = require('../services/repository');
 
 /* GET home page. */
 router.get('/', function (req, res) {
---
+++ WordOfDay/services/repository.js
@@ -0,0 +1,14 @@
+﻿var config = require('../config.json');
+var mongoose = require('mongoose');
+mongoose.connect(config.database.connection);
+
+var db = mongoose.connection;
+db.on('error', function (err) { 
+    console.error(err);
+});
+db.once('open', function () { 
+    console.log('connected to ' + db.name + ' database on ' + db.host);
+});
+
+module.exports = db;
+
